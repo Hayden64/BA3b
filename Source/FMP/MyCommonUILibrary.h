5baf85de0e6c2bd65b1027379f593d1151832ba6
Hayden64 <hayden1ward@yahoo.co.uk>
2024-12-08 19:05:15 +0000

    Honestly dont know whats going on

---
+++ Source/FMP/MyCommonUILibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUILibrary.h"
+#include "MyCommonUILibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FMP_API UMyCommonUILibrary : public UCommonUILibrary
+{
+	GENERATED_BODY()
+	
+};
