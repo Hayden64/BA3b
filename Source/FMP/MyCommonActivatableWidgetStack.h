3c716773ab517ed7cc93eb0a3fc5ba77574d4cb0
Hayden64 <hayden1ward@yahoo.co.uk>
2024-12-16 18:52:39 +0000

    ?

---
+++ Source/FMP/MyCommonActivatableWidgetStack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/CommonActivatableWidgetContainer.h"
+#include "MyCommonActivatableWidgetStack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FMP_API UMyCommonActivatableWidgetStack : public UCommonActivatableWidgetStack
+{
+	GENERATED_BODY()
+	
+};
