1a3edbdb4815a790552dea1ac85a39870bac6ed1
Hayden64 <hayden1ward@yahoo.co.uk>
2025-02-18 17:41:02 +0000

    Started working on C++
    Added:
    - Button
    Need:
    - Player Controller
    - Player Character (Camera movement for building)
    
    Just replace the blueprint code with C++ where possible.

---
+++ Source/FMP/BaseGameController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseGameController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FMP_API ABaseGameController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
